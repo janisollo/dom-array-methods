6af3f43d6766c1a6b7a7c83146375346e5f613e7
janisollo <sollo.jani@gmail.com>
2022-06-07 16:07:29 +0700

    random user fetched and money function created

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  console.log(data);
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last} ${user.email} ${user.phone}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+}
